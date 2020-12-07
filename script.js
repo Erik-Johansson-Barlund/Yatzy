f99b5b127cecf9e16a2bc6a3982e54dbc8399ade
Erik Johansson Bärlund <erik163@gmail.com>
2020-12-07 13:02:29 +0100

    En möjlig lösning på uppgiften

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+document.addEventListener('DOMContentLoaded', function (e) {
+	let btn = document.getElementById('btn');
+
+	btn.addEventListener('click', function (e) {
+		let values = [];
+		let player_1_ones = document.getElementById('player_1_ones').value;
+		let player_1_twos = document.getElementById('player_1_twos').value;
+		let player_1_threes = document.getElementById('player_1_threes').value;
+		let player_1_fours = document.getElementById('player_1_fours').value;
+		let player_1_fives = document.getElementById('player_1_fives').value;
+		let player_1_sixes = document.getElementById('player_1_sixes').value;
+
+		values = [
+			player_1_ones,
+			player_1_twos,
+			player_1_threes,
+			player_1_fours,
+			player_1_fives,
+			player_1_sixes,
+		];
+		let player_1_sum = document.getElementById('player_1_sum');
+		let player_1_bonus = document.getElementById('player_1_bonus');
+
+		//console.log(player_1_ones);
+		countDiceValues(values);
+	});
+});
+function countDiceValues(values) {
+	let result = 0;
+
+	for (let value = 0; value < values.length; value++) {
+		let current = Number(values[value]) * (value + 1);
+		result += current;
+	}
+	player_1_sum.innerHTML = result;
+}
+/*		
 class Die {
 	constructor(num) {
 		this.no = num;
@@ -28,3 +65,4 @@ class Dice {
 }
 
 //let set = new Dice();
+*/
